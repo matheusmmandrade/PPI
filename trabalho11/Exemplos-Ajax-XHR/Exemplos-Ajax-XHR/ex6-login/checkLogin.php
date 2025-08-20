fa3a4cde209f9970086550d1b4633063e0c35f4c
Matheus Matos <matheusmmandrade@gmail.com>
2025-08-20 11:20:37 -0300

    add: ex6 trab11

--- trabalho11/Exemplos-Ajax-XHR/Exemplos-Ajax-XHR/ex6-login/checkLogin.php
+++ trabalho11/Exemplos-Ajax-XHR/Exemplos-Ajax-XHR/ex6-login/checkLogin.php
@@ -12,14 +12,22 @@ class LoginResult
   }
 }
 
+
+// classe para definir o json
+
+
 $email = $_POST['email'] ?? '';
 $senha = $_POST['senha'] ?? '';
 
 // Validação simplificada para fins didáticos. Não faça isso!
 if ($email == 'fulano@mail.com' && $senha == '123456')
   $loginResult = new LoginResult(true, 'home.html');
+// se a senha for 123456, redireciona para a página de sucesso
 else
   $loginResult = new LoginResult(false, '');
+
+// se a senha for diferente de 123456, não acontece nada, o novo local é nulo, então continua na página de login
+
 
 header('Content-type: application/json');
 echo json_encode($loginResult);
