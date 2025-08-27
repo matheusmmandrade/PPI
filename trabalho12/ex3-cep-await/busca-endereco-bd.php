ae0e9c7fe4c196dd8267051d520206f65ff9e643
Matheus Matos <matheusmmandrade@gmail.com>
2025-08-27 17:31:22 -0300

    Trab12 ex3 c

--- trabalho12/ex3-cep-await/busca-endereco-bd.php
+++ trabalho12/ex3-cep-await/busca-endereco-bd.php
@@ -5,35 +5,23 @@ $pdo = mysqlConnect();
 
 $cep = $_GET['cep'] ?? '';
 
-try
-{
+try {
   $stmt = $pdo->prepare(
     <<<SQL
-    SELECT cep, logradouro, bairro, cidade
+    SELECT cep, logradouro AS rua, bairro, cidade
     FROM endereco
     WHERE cep = ?
     SQL
   );
 
-  $stmt->execute([$cep]);  
+  $stmt->execute([$cep]);
   $endereco = $stmt->fetch(PDO::FETCH_OBJ);
   header('Content-Type: application/json; charset=utf-8');
-  echo json_encode($endereco);
-} 
-catch (Exception $e) {
+  echo json_encode($endereco ?: new stdClass());
+} catch (Exception $e) {
   exit("Falha ao buscar endereco: " . $e->getMessage());
 }
 
-/*
-CREATE TABLE endereco
-(
-  cep char(9) PRIMARY KEY,
-  logradouro varchar(30),
-  bairro varchar(30),
-  cidade varchar(30)
-);
 
-INSERT INTO endereco VALUES ('38400-100', 'Av Floriano Peixoto', 'Centro', 'Uberlândia');
-INSERT INTO endereco VALUES ('38400-200', 'Rua Tiradentes', 'Fundinho', 'Uberlândia');
-INSERT INTO endereco VALUES ('38408-100', 'Av. João Naves de Ávila, 2121', 'Santa Mônica', 'Uberlândia');
-*/
+
+
