f3beb0a9c88d826308447c2243bfd1a7d8a05606
Matheus Matos <matheusmmandrade@gmail.com>
2025-09-04 08:14:02 -0300

    Conexao MySQL

--- trabalho13/ex02-session-login/conexaoMysql.php
+++ trabalho13/ex02-session-login/conexaoMysql.php
@@ -2,21 +2,20 @@
 
 function mysqlConnect()
 {
-  $db_host = "exemplo.com";
-  $db_username = "ppi";
-  $db_password = "ppi";
-  $db_name = "ppi";
+  $db_host = "sql208.infinityfree.com";
+  $db_username = "if0_39210014";
+  $db_password = "pouM4xu1ngY";
+  $db_name = "if0_39210014_ppi";
 
   $options = [
-    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+    PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
   ];
 
   try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
     return $pdo;
-  } 
-  catch (Exception $e) {
+  } catch (Exception $e) {
     exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
   }
 }
